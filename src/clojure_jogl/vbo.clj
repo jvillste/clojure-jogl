64852f3f42a6effa14a3d600efed9893085b91bf
Jukka Villstedt <jukka.villstedt@gmail.com>
2011-08-21 01:23:20 +0300

    x

--- src/clojure_jogl/vbo.clj
+++ src/clojure_jogl/vbo.clj
@@ -1,4 +1,4 @@
-(ns clojure-jog.vbo
+(ns clojure-jogl.vbo
   (:import [java.nio IntBuffer FloatBuffer]
            [java.awt Frame BorderLayout Font]
            [java.awt.event WindowListener WindowAdapter KeyListener KeyEvent]
@@ -8,15 +8,19 @@
            [javax.swing JFrame]
            [java.nio FloatBuffer]))
 
-(defn generate-vbo [gl]
+(def *buffer-ids* (atom {}))
+
+(defn generate-vbo [gl key]
   (let [id-array (int-array 1)]
     (.glGenBuffers gl 1 id-array 0)
+    (reset! *buffer-ids* (assoc @*buffer-ids* key (first id-array)))
     (println (str "generated buffer " (first id-array)))
     (first id-array)))
 
-(def *buffer-id*)
+(def *vertex-buffer* (atom nil))
 
-(def *vertexes*)
+
+(defn get-vbo [key] (key @*buffer-ids*))
 
 (def canvas (doto (GLCanvas. (GLCapabilities. (GLProfile/get GLProfile/GL2)))
               (.setSize 400 400)
@@ -44,59 +48,79 @@
 
                                          (.glEnableClientState gl GL2/GL_VERTEX_ARRAY)
 
-                                         (def *buffer-id* (generate-vbo gl))
 
-                                         (.glBindBuffer gl GL2/GL_ARRAY_BUFFER *buffer-id*)
+                                         ;; Create the vertex bufer
+
+                                         (generate-vbo gl :vertex-buffer)
+
                                          (let [width (.getWidth drawable)
                                                height (.getHeight drawable)]
 
-                                           (def *vertexes* (float-array [0 0 0
-                                                                         width 0 0
-                                                                         (/ width 2)  height 0]))
+                                           (reset! *vertex-buffer* (float-array [0 0 0
+                                                                                width 0 0
+                                                                                (/ width 2)  height 0]))
+                                           (.glBindBuffer gl GL2/GL_ARRAY_BUFFER (get-vbo :vertex-buffer))
                                            (.glBufferData gl
                                                           GL2/GL_ARRAY_BUFFER
-                                                          (alength *vertexes*)
-                                                          (FloatBuffer/wrap *vertexes*)
+                                                          (alength @*vertex-buffer*)
+                                                          (FloatBuffer/wrap @*vertex-buffer*)
+                                                          GL2/GL_STATIC_DRAW))
+
+
+
+                                         ;; Create index buffer
+
+                                         (generate-vbo gl :index-buffer)
+
+                                         (.glBindBuffer gl GL2/GL_ELEMENT_ARRAY_BUFFER (get-vbo :index-buffer))
+                                         (let [index-buffer (int-array [0 1 2])]
+                                           (.glBufferData gl
+                                                          GL2/GL_ELEMENT_ARRAY_BUFFER
+                                                          (alength index-buffer)
+                                                          (IntBuffer/wrap index-buffer)
                                                           GL2/GL_STATIC_DRAW))))
+
 
 
                                      (dispose [#^GLAutoDrawable drawable]
                                        (println "Dispose")
                                        (.glDeleteBuffers (.getGL2 (.getGL drawable))
                                                          1
-                                                         (int-array *buffer-id*)
+                                                         (int-array (get-vbo :vertex-buffer))
                                                          0))
 
                                      (display  [#^GLAutoDrawable drawable]
 
                                        (let [width (.getWidth drawable)
                                              height (.getHeight drawable)]
-                                         (println (str "display " width " " height " buffer id: " *buffer-id*))
+                                         (println (str "display " width " " height " buffer id: " (get-vbo :vertex-buffer)))
                                          (doto (.getGL2 (.getGL drawable))
                                            (.glClear GL2/GL_COLOR_BUFFER_BIT)
                                            (.glLoadIdentity)
 
                                            ;; (.glColor3f 0.0 1.0 0.0)
                                            ;; (.glBegin GL2/GL_TRIANGLES)
-                                           ;; (.glVertex3f (aget *vertexes* 0)
-                                           ;;              (aget *vertexes* 1)
-                                           ;;              (aget *vertexes* 2))
-                                           ;; (.glVertex3f (aget *vertexes* 3)
-                                           ;;              (aget *vertexes* 4)
-                                           ;;              (aget *vertexes* 5))
-                                           ;; (.glVertex3f (aget *vertexes* 6)
-                                           ;;              (aget *vertexes* 7)
-                                           ;;              (aget *vertexes* 8))
+                                           ;; (.glVertex3f (aget *vertex-buffer* 0)
+                                           ;;              (aget *vertex-buffer* 1)
+                                           ;;              (aget *vertex-buffer* 2))
+                                           ;; (.glVertex3f (aget *vertex-buffer* 3)
+                                           ;;              (aget *vertex-buffer* 4)
+                                           ;;              (aget *vertex-buffer* 5))
+                                           ;; (.glVertex3f (aget *vertex-buffer* 6)
+                                           ;;              (aget *vertex-buffer* 7)
+                                           ;;              (aget *vertex-buffer* 8))
 
                                            ;; (.glEnd)
 
                                            (.glColor3f 1.0 0.0 0.0)
                                            ;;(.glEnableClientState GL2/GL_VERTEX_ARRAY)
 
-                                           (.glBindBuffer GL2/GL_ARRAY_BUFFER *buffer-id*)
-
+                                           (.glBindBuffer GL2/GL_ARRAY_BUFFER (get-vbo :vertex-buffer))
                                            (.glVertexPointer 3 GL2/GL_FLOAT 0 (long 0))
-                                           (.glDrawArrays GL2/GL_TRIANGLES 0 9)
+
+                                           (.glBindBuffer GL2/GL_ARRAY_BUFFER (get-vbo :index-buffer))
+                                           (.glDrawElements GL2/GL_TRIANGLES 3 GL2/GL_UNSIGNED_INT (long 0))
+;;                                           (.glDrawArrays GL2/GL_TRIANGLES 0 9)
 
                                            (.glFlush)
 
